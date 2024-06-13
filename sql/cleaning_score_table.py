164cfbdd5d2dab99773ec4b0069546269a0a9ef2
Dafydd Lloyd <DLloyd@spartaglobal.com>
2024-06-13 14:10:38 +0100

    Corrected weeks to be entered as int instead of string

--- sql/cleaning_score_table.py
+++ sql/cleaning_score_table.py
@@ -7,7 +7,7 @@ connection_string = (
     'Server=127.0.0.1;'
     'Database=FinalProject;'
     'UID=sa;'
-    'PWD=yourStrong(!)Password;'
+    'PWD=Ducks123;'
     'Trusted_Connection=no;'
 )
 connection_url = sa.engine.URL.create(
@@ -19,8 +19,9 @@ engine = sa.create_engine(connection_url, fast_executemany=True)
 academy_data = pd.read_sql("SELECT * FROM Academy_CSV",engine)
 
 
-academy_data_melted = academy_data.melt(id_vars=['Category', 'Stream', 'Date', 'ACname', 'trainer'], var_name='variable', value_name='Score')
-academy_data_melted['Week'] = academy_data_melted['variable'].str.extract(r'_(W\d+)$')
+academy_data_melted = academy_data.melt(id_vars=['category', 'stream', 'date', 'name', 'trainer'], var_name='variable', value_name='Score')
+academy_data_melted['Week'] = academy_data_melted['variable'].str.extract(r'W(\d+)$')
+academy_data_melted['Week'] = academy_data_melted['Week'].apply(int)
 academy_data_melted['Behaviour'] = academy_data_melted['variable'].str.extract(r'^(\w+)_')
 academy_data_melted = academy_data_melted.drop(columns=['variable'])
 
