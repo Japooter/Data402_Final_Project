151ca85a27910d98a3e7259db4b8026a11eaab55
Kyrun Philip-Lessells <kphilip-lessells@spartaglobal.com>
2024-06-13 11:12:33 +0100

    Added foreign key constraints to reorder_applicants_columns

--- sql/Reorder_Applicants_Columns.sql
+++ sql/Reorder_Applicants_Columns.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS TempTable
+ 
 SELECT
     Applicant_ID,
     name AS "Applicant_Name",
@@ -17,9 +19,22 @@ SELECT
     presentation_Score AS "Presentation_Score",
     result AS "Result",
     Talent_Coordinator_ID
-INTO TempTable
-FROM Applicants;
-ORDER BY Applicant_ID
+FROM Applicants
 
 EXEC sp_rename 'Applicants', 'Dropped'
 EXEC sp_rename 'TempTable', 'Applicants'
+
+ALTER TABLE Applicants
+ADD CONSTRAINT Applicant_ID PRIMARY KEY (Applicant_ID)
+
+ALTER TABLE Applicants
+ADD FOREIGN KEY (Address_ID) REFERENCES Address(Address_ID)
+
+ALTER TABLE Applicants
+ADD FOREIGN KEY (Sparta_Day_ID) REFERENCES Sparta_Day(Sparta_Day_ID)
+
+ALTER TABLE Applicants
+ADD FOREIGN KEY (Uni_ID) REFERENCES Uni(Uni_ID)
+
+ALTER TABLE Applicants
+ADD FOREIGN KEY (Talent_Coordinator_ID) REFERENCES Talent_Coordinator(Talent_Coordinator_ID)
